503385fced8536e25fe61324be8b541656023eee
abramsea <abramson.ea@gmail.com>
2020-01-29 16:05:02 +0300

    pre-release

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,13 @@
 menu.onclick = function openHiddenMenu() {
     var x = document.getElementById('topnav');
 
-    if (x.className === "nav") {
-        $("#topnav").addClass(" responsive")
-        $("#menu").html("&#215;");
+    if (x.className === 'nav') {
+        $('#topnav').addClass(' responsive')
+        $('#menu').html('&#215;');
+        $('.welcome').addClass(' hidden');
     } else {
-        x.className = "nav";
-        $("#menu").html("&#9776;");
+        x.className = 'nav';
+        $('.welcome').removeClass(' hidden');
+        $('#menu').html('&#9776;');
     }
 }
